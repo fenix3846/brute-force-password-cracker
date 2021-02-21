2c5600f35c52bfc72c40d33c7cd5f118dd63dc1c
fenix3846 <77797155+fenix3846@users.noreply.github.com>
2021-02-21 19:57:59 +0530

    Add files via upload

---
+++ password.py
@@ -0,0 +1,18 @@
+import random
+import pyautogui 
+
+chars = "abcdefghijklmnopqrstuvwxyz123456789"
+chars_list = list(chars)
+
+password = pyautogui.password("enter a password: ")
+
+guess_password = ""
+
+while(guess_password != password):
+    guess_password = random.choices(chars_list , k=len(password))
+
+    print("<==================" , str(guess_password) , "==================>"  )
+
+    if (guess_password == list(password)):
+        print("your password is: " + "".join(guess_password) )
+        break
